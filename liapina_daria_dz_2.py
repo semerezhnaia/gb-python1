7d32903a5aed6612539b19fc0b2bc43d0f710a17
semerezhnaia <semerezhnaya@mail.ru>
2022-03-14 23:16:11 +0300

    answer

--- liapina_daria_dz_2.py
+++ liapina_daria_dz_2.py
@@ -1,14 +1,14 @@
 list_1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 i = 0
 while i < len(list_1):
-    cnt = list_1[i]
-    if cnt[-1].isdigit():
-        if cnt.startswith(('+')):
+    count = list_1[i]
+    if count[-1].isdigit():
+        if count.startswith(('+')):
             len_str = 3
         else:
             len_str = 2
-        if len(cnt) < len_str:
-            cnt = cnt.zfill(len_str)
-            list_1[i] = cnt
+        if len(count) < len_str:
+            count = count.zfill(len_str)
+            list_1[i] = count
     i += 1
 print(" ".join(list_1))
--- liapina_daria_dz_4.py
+++ liapina_daria_dz_4.py
@@ -1,5 +1,5 @@
-list_1 = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for first_name in list_1:
+list_name = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+for first_name in list_name:
     first_name_1 = first_name.split()[-1]
     first_name_1 = first_name_1.capitalize()
     print('Привет,', first_name_1, '!')
